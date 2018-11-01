060667983fb89a599372ff7dc15aa77e4dc9bbff
jacekstamm <jacekstamm@gmail.com>
2018-11-01 22:07:34 +0100

    1

--- kodilla-stream/src/test/java/com/kodilla/stream/beautifier/BeautifierTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/beautifier/BeautifierTestSuite.java
@@ -1,4 +1,22 @@
 package com.kodilla.stream.beautifier;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.PoemDecorator;
+import org.junit.*;
+import static org.mockito.Mockito.*;
+
 public class BeautifierTestSuite {
+    @Test
+    public void testShouldPrintTheSameText() {
+        //Given
+        PoemDecorator poemDecoratorMock = mock(PoemDecorator.class);
+        poemDecoratorMock.decorate("Giełda Słów");
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        //When
+        poemBeautifier.beautify("Giełda Słów", text -> text + text);
+
+        //Then
+
+    }
 }
