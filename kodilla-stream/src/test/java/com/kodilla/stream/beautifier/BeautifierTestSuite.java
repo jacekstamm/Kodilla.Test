89906efedfec506629e3186f330753cc6310e672
jacekstamm <jacekstamm@gmail.com>
2018-11-02 17:16:00 +0100

    Testy do zadania 7.1

--- kodilla-stream/src/test/java/com/kodilla/stream/beautifier/BeautifierTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/beautifier/BeautifierTestSuite.java
@@ -9,14 +9,21 @@ public class BeautifierTestSuite {
     @Test
     public void testShouldPrintTheSameText() {
         //Given
-        PoemDecorator poemDecoratorMock = mock(PoemDecorator.class);
-        poemDecoratorMock.decorate("Giełda Słów");
         PoemBeautifier poemBeautifier = new PoemBeautifier();
 
+
         //When
-        poemBeautifier.beautify("Giełda Słów", text -> text + text);
+        String poem1 = poemBeautifier.beautify("Litwo Ojczyzno Moja!", text -> text + " " + text.toUpperCase());
+        String poem2 = poemBeautifier.beautify("Giełda Słów", text -> "ABC " + text + " ABC");
+        String poem3 = poemBeautifier.beautify("Zasady wywierania wpływu na ludzi", text -> text.toLowerCase());
+        String poem4 = poemBeautifier.beautify("Przeszkoda czy wyzwanie?", text -> "Tytuł: " + text + " Rok wydania: 2016");
+        String poem5 = poemBeautifier.beautify("Jazda! ", text -> text + text + text + text.toUpperCase());
 
         //Then
-
+        Assert.assertEquals("Litwo Ojczyzno Moja! LITWO OJCZYZNO MOJA!", poem1);
+        Assert.assertEquals("ABC Giełda Słów ABC", poem2);
+        Assert.assertEquals("zasady wywierania wpływu na ludzi", poem3);
+        Assert.assertEquals("Tytuł: Przeszkoda czy wyzwanie? Rok wydania: 2016", poem4);
+        Assert.assertEquals("Jazda! Jazda! Jazda! JAZDA! ", poem5);
     }
 }
